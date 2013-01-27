2b0f14bda63ce9dac45248f095459f0ef146f9c9
Richard Boss <per.richard.boss@gmail.com>
2013-01-27 03:08:48 +0100

    added new 'match history' and 'match details' pages

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express();
+var http = require('http');
 var port = process.env.PORT || 2013;
 
 //var fileServer = new static.Server('./public');
@@ -13,11 +14,65 @@ app.configure(function(){
   app.use(express.static(__dirname + '/public'));  
 });
 
-app.get('/', function(req, res) {
-	res.render('index', { message: "laguna"});
+var options = {
+	host: "api.steampowered.com",
+	port: 80,
+	path: "/IDOTA2Match_570/GetMatchHistory/V001/?key=B957D9310F4F87536FFDD2EBA0B803C4"
+};
+var optionsDetails = function(matchId) {
+	return {
+		host: "api.steampowered.com",
+		port: 80,
+		path: "/IDOTA2Match_570/GetMatchDetails/V001/?key=B957D9310F4F87536FFDD2EBA0B803C4&match_id=" + matchId
+	};
+};
+
+app.get(['/', '/matches'], function(req, res) {
+	getMatchhistory(function(data) {
+		res.render('index', { message: "laguna", matchHistory: data });
+	});	
+});
+app.get('/match/:id', function(req, res) {
+	var id = req.params.id;
+	getMatchDetails(id, function(data) {
+		res.render('match', { id: id, match_details: data });	
+	});	
 });
 
+var getMatchDetails = function(matchId, callback) {
+	http.get(optionsDetails(matchId), function(res) {
+		var data = '';
+
+		res.on('data', function(chunk) {
+			data += chunk;
+		});
+		res.on('end', function() {
+			console.log('End of get request for match details.');
+			callback(JSON.parse(data).result);
+		});
+	});
+};
+var getMatchhistory = function(callback) {
+	http.get(options, function(res) {
+		var data = '';
+
+		res.on('data', function(chunk) {
+			data += chunk;
+		});
+
+		res.on('end', function() {
+			console.log('End of get request for match history.');
+			callback(JSON.parse(data).result);
+		});
+
+	}).on("error", function(e){
+  		console.log("Got error while retrieving match history: " + e.message);
+	});
+};
+
+
 app.listen(port);
+
 
 
 console.log('Server started at port: ' + port);
