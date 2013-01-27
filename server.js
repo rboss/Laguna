f40b539b3645c32728c021576529199256b49d06
Richard Boss <per.richard.boss@gmail.com>
2013-01-27 01:52:07 +0100

    added express to serve files added jade as view engine

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-var http = require('http');
-var static = require('node-static');
-
-var fileServer = new static.Server('./public');
+var express = require('express')
+var app = express();
 var port = process.env.PORT || 2013;
-var server = http.createServer(function(req, res) {
-	req.addListener('end', function() {
-		fileServer.serve(req, res);
-	});
-}).listen(port);
+
+//var fileServer = new static.Server('./public');
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));  
+});
+
+app.get('/', function(req, res) {
+	res.render('index', { message: "laguna"});
+});
+
+app.listen(port);
+
 
 console.log('Server started at port: ' + port);
