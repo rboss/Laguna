d38a4e87713c7b13c0f4188634ae4c37e541a0a7
Richard Boss <per.richard.boss@gmail.com>
2013-02-18 23:25:49 +0100

    added more mocha tests for the database

--- test/lagunadb.tests.js
+++ test/lagunadb.tests.js
@@ -1,14 +1,59 @@
 
-var db = require('../lagunadb').init();
+describe('LagunaDB', function() {
+	var db = require('../lagunadb').init();
 
-describe('LagunaDb', function() {
-	it('clears all matches', function(done) {
-
-		db.insertMatch({id: 1300, name:"test"});
+	beforeEach(function() {
 		db.clearMatches();
-		db.findMatches(function(matches) {
-			matches.length.should.equal(0);
-			done();
+		db.insertMatch({ match_id: 1300, name:"test1" });
+		db.insertMatch({ match_id: 1301, name:"test2" });
+		db.insertMatch({ match_id: 1302, name:"test3" });
+	});
+	
+	describe('clearMatches', function() {
+
+		beforeEach(function() {
+			db.clearMatches();
+		});
+
+		it('clears all matches', function(done) {			
+			db.findMatches(function(matches) {
+				matches.length.should.equal(0);
+				done();
+			});			
 		});
 	});
+	describe('insertMatch', function() {
+
+		it('should increase match count with one', function(done) {
+			db.insertMatch({ match_id: 1400, name:"test4" });
+			db.findMatches(function(matches) {
+				console.log(matches.length);
+				matches.length.should.equal(4);
+				done();
+			});
+		});
+		it('should not increase match count if insert with existing match_id', function(done) {
+			db.insertMatch({ match_id: 1300, name:"test4" });
+			db.findMatches(function(matches) {
+				console.log(matches.length);
+				matches.length.should.equal(3);
+				done();
+			});
+		});		
+		it('should return error code 11000 [duplicate key error index] if insert with existing match_id', function(done) {
+			db.insertMatch({ match_id: 1300, name:"test4" }, function(err) {
+				err.code.should.equal(11000);
+				done();
+			});
+
+		});		
+	});	
+	describe('findMatches', function() {
+		it('should find all matches', function(done) {
+			db.findMatches(function(matches) {
+				matches.length.should.equal(3);
+				done();
+			});
+		});
+	});	
 });
